a0c5afc804afc44d07416aac201c95b867a0baaa
QuangTran013 <113117168+daVinc3nt@users.noreply.github.com>
2024-04-30 22:30:58 +0700

    ok

--- pages/log.tsx
+++ pages/log.tsx
@@ -1,7 +1,6 @@
 import type { NextPage } from "next";
 import { LoginPage } from "@/components/Login/LoginPage";
 // import Footer from "../components/Footer/Footer";
-import MobileLog from "@/components/Login/LoginPage/MobileLog";
 const LogForm: NextPage = () => {
   return (
     <>
